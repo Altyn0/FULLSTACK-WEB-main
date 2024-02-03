9b16858881cbba22c3c6d37eba92a1999fba8b3f
Altyn0 <96158393+Altyn0@users.noreply.github.com>
2024-02-03 17:52:36 +0700

    day5

---
+++ day5/controller/user.controller.js
@@ -0,0 +1,43 @@
+import express from "express";
+
+const userController = express.Router();
+
+userController.get("", (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+userController.get(":userId", (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+userController.post("", (req, res) => {
+    try {
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  });
+
+  userController.put(":userId", (req, res) => {
+    try {
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  });
+
+  userController.delete(":postId", (req, res) => {
+    try {
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  });
